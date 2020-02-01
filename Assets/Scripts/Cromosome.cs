374e254107064e5b0de114c22fdb9208e3906ae6
Jesús Villar <jesusferminvillar@gmail.com>
2020-02-01 14:26:42 +0100

    First commit

---
+++ Assets/Scripts/Cromosome.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using ProceduralNoiseProject;
+
+[System.Serializable]
+public class Cromosome 
+{
+    // The size of voxel array.
+    static int width    = 32;
+    static int height   = 2;
+    static int length   = 32;
+
+    public float       [] voxels;
+    float              [] mutations;
+
+    float       mutationChance = 0.2f;
+    List<Vector3> navMeshVertices = new List<Vector3>();
+    float       longestPathDistance = 0.0f;
+
+    public Cromosome()
+    {
+        voxels      = new float [width * height * length];
+        mutations   = new float [voxels.Length];
+        
+        int mutationSeed = UnityEngine.Random.Range(0, int.MaxValue);
+        
+        INoise perlin        = new PerlinNoise(mutationSeed, 1.0f);
+        FractalNoise fractal = new FractalNoise(perlin, 2, 2.0f);
+
+        //Fill voxels with values. Im using perlin noise but any method to create voxels will work.
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int z = 0; z < length; z++)
+                        {
+                            float fx = x / (width - 1.0f);
+                            float fy = y / (height - 1.0f);
+                            float fz = z / (length - 1.0f);
+
+                            int idx = x + y * width + z * width * height;
+
+                            mutations[idx] = fractal.Sample3D(fx, fy, fz);
+                        }
+                    }
+                }
+    }
+
+
+    /// <summary>
+	/// Recombine two cromosomes obtaining a new cromosome with random 
+	/// </summary>
+    public Cromosome Recombine(Cromosome other)
+    {
+        Cromosome child = new Cromosome();
+        int chunkSize = voxels.Length / 8;
+
+        for(int i = 0; i < voxels.Length; i += chunkSize)
+        {
+            
+            float combinatorial = UnityEngine.Random.Range(0.0f, 1.0f);
+            
+            if (combinatorial <= mutationChance)
+            {
+                for (int j = 0; j < chunkSize; ++j)
+                {
+                    child.GetVoxels()[i + j]   = Mutation(i+j);
+                }
+            }
+            else if (combinatorial <= mutationChance + ((1-mutationChance) / 2))
+            {
+                for (int j = 0; j < chunkSize; ++j)
+                {
+                    child.GetVoxels()[i + j]   = this.GetVoxels()[i + j];
+                }
+            }
+            else 
+            {
+                for (int j = 0; j < chunkSize; ++j)
+                {
+                    child.GetVoxels()[i+j]   = other.GetVoxels()[i+j];
+                }
+            }
+        }
+
+        return child;
+    }
+
+    public float [] GetVoxels() => voxels;
+    public int      GetWidth () => width;
+    public int      GetHeight() => height;
+    public int      GetLength() => length;
+    public void     SetVoxels(float [] voxels) => this.voxels = voxels;
+    public float    Mutation(int i) => mutations[i];
+    public float    GetLongestPathDistance() => longestPathDistance;
+    public void     AddVertex(Vector3 vertex) => navMeshVertices.Add(vertex);
+
+    public void CalculateLongestDistance(NavMeshAgent agent)
+    {
+        for(int i = 0; i < navMeshVertices.Count - 1 ; ++i)
+        {
+            for(int j = i; j < navMeshVertices.Count; ++j)
+            {
+                NavMeshPath path = new NavMeshPath();
+
+                if(NavMesh.CalculatePath(navMeshVertices[i], navMeshVertices[j], NavMesh.AllAreas, path))
+                {
+                    agent.transform.position = navMeshVertices[i];
+                    agent.SetDestination(navMeshVertices[j]);
+                    
+                    if(agent.remainingDistance > longestPathDistance && agent.remainingDistance != Mathf.Infinity)
+                    {
+                        longestPathDistance = agent.remainingDistance;
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using TMPro;
+using System.Linq;
+using NavMeshBuilder = UnityEngine.AI.NavMeshBuilder;
+
+using ProceduralNoiseProject;
+
+    //[ExecuteInEditMode]
+    public class Example : MonoBehaviour
+    {
+
+        public Material m_material;
+        public const int    numberOfMeshes      = 10;
+        public float        visualizationTime   = 0.5f;
+        public int          survivors           = 2;
+        public int          [] seeds            = new int       [numberOfMeshes];
+        Cromosome           [] cromosomes       = new Cromosome [numberOfMeshes];
+        public Transform    [] pivots           = new Transform [numberOfMeshes];
+        List<GameObject>    meshes              = new List<GameObject>();
+        MarchingCubes       marching            = new MarchingCubes();
+        public NavMeshAgent agent;
+
+        public TextMeshProUGUI GenerationText;
+        int                    generationIndex        = 0;
+        float                  bestHistorical         = float.MinValue;
+        Cromosome           [] bestCromosomesInOrder  = new Cromosome [numberOfMeshes];
+        
+        void Start()
+        {
+
+            //Surface is the value that represents the surface of mesh
+            //For example the perlin noise has a range of -1 to 1 so the mid point is where we want the surface to cut through.
+            //The target value does not have to be the mid point it can be any value with in the range.
+            marching.Surface = 0.0f;
+            GenerationText.text = "Generation: " + generationIndex;
+            
+            FirstGeneration();
+        }
+
+        public void FirstGeneration()
+        {
+            for(int i = 0; i < numberOfMeshes; ++i)
+            {
+                INoise perlin        = new PerlinNoise(seeds[i], 1.0f);
+                FractalNoise fractal = new FractalNoise(perlin, 2, 2.0f);
+
+                Cromosome cromosome = new Cromosome();
+
+                int      width       = cromosome.GetWidth();
+                int      height      = cromosome.GetHeight();
+                int      length      = cromosome.GetLength();
+                float [] voxels      = new float [width * height * length];
+
+                //Fill voxels with values. Im using perlin noise but any method to create voxels will work.
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int z = 0; z < length; z++)
+                        {
+                            float fx = x / (width - 1.0f);
+                            float fy = y / (height - 1.0f);
+                            float fz = z / (length - 1.0f);
+
+                            int idx = x + y * width + z * width * height;
+
+                            voxels[idx] = fractal.Sample3D(fx, fy, fz);
+                        }
+                    }
+                }
+
+                cromosome.SetVoxels(voxels);
+                pivots[i].name = "--";
+                cromosomes[i] = cromosome;
+                CreateMesh(cromosome, i);
+            }
+            SetNewParents();
+        }
+
+        public void SetNewParents()
+        {
+            Evaluate();
+            Invoke("NextGeneration", visualizationTime);
+        }
+
+        public void NextGeneration()
+        {
+            ++generationIndex;
+            GenerationText.text = "Generation: " + generationIndex;
+
+            // Clear the meshes
+            foreach(GameObject go in meshes)
+            {
+                Destroy(go);
+            }
+
+            meshes.Clear();
+
+            // Create the childs cromosomes
+            for(int i = 0; i < cromosomes.Length; ++i)
+            {
+                pivots[i].name = "--";
+                
+                // Choose the parents randomnly between the survivors
+                int index1;
+                int index2;
+                
+                index1 = UnityEngine.Random.Range(0, survivors);
+                
+                do
+                {
+                    index2 = UnityEngine.Random.Range(0, survivors);
+                }while (index2 == index1);
+
+                cromosomes[i] = bestCromosomesInOrder[index1].Recombine(bestCromosomesInOrder[index2]);
+                
+                CreateMesh(cromosomes[i], i);
+            }
+
+            SetNewParents();
+        }
+
+        public void CreateMesh(Cromosome cromosome, int index)
+        {
+            List<Vector3> verts = new List<Vector3>();
+            List<int> indices   = new List<int>();
+
+            float [] voxels      = cromosome.GetVoxels();
+            int      width       = cromosome.GetWidth();
+            int      height      = cromosome.GetHeight();
+            int      length      = cromosome.GetLength();
+
+            //The mesh produced is not optimal. There is one vert for each index.
+            //Would need to weld vertices for better quality mesh.
+            marching.Generate(voxels, width, height, length, verts, indices);
+            
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(verts);
+            mesh.SetTriangles(indices, 0);
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            GameObject go = new GameObject("Mesh");
+            go.AddComponent<MeshFilter>();
+            go.AddComponent<MeshRenderer>();
+            go.GetComponent<Renderer>().material = m_material;
+            go.GetComponent<MeshFilter>().mesh = mesh;
+            go.transform.SetParent(pivots[index]);
+            go.isStatic = true;
+            go.transform.localPosition = new Vector3(-width * 0.5f, 0, -length * 0.5f );
+            go.AddComponent<NavMeshSourceTag>();
+
+            meshes.Add(go);
+        }
+
+        public void Evaluate()
+        {
+            var navMesh = NavMesh.CalculateTriangulation();
+            Vector3[] vertices = navMesh.vertices;
+            
+            // Check what navmesh belongs to each cromosome
+            for(int i = 0; i < vertices.Length; ++i)
+            {
+                float lowerDistance = float.MaxValue;
+                int owner           = 0;
+                
+                for(int j = 0; j < pivots.Length; ++j)
+                {
+                    float distance = Vector3.Distance(vertices[i], pivots[j].position);
+                    if(distance < lowerDistance)
+                    {
+                        lowerDistance = distance;
+                        owner = j;
+                    }
+                }
+
+                cromosomes [owner].AddVertex(vertices[i]);
+            } 
+
+            List<Cromosome> OrderedCromosomes = new List<Cromosome>();
+
+            for(int i = 0; i < cromosomes.Length; ++i)
+            {
+                cromosomes[i].CalculateLongestDistance(agent);
+                OrderedCromosomes.Add(cromosomes[i]);
+            }
+
+            OrderedCromosomes.OrderByDescending(go => go.GetLongestPathDistance());
+            bestCromosomesInOrder = OrderedCromosomes.ToArray();
+
+            if(bestCromosomesInOrder[0].GetLongestPathDistance() > bestHistorical)
+            {
+                bestHistorical = bestCromosomesInOrder[0].GetLongestPathDistance();
+            }
+
+        }
+
+    }
