054aa4b2a67de88969053e9a5bb02d7f4bd21728
Jesús Villar <jesusferminvillar@gmail.com>
2020-02-02 19:13:11 +0100

    Documentation

--- Assets/Scripts/Cromosome.cs
+++ Assets/Scripts/Cromosome.cs
@@ -1,4 +1,33 @@
-﻿using System.Collections;
+﻿/*
+ * File: Cromosome.cs
+ * File Created: Sunday, 2nd February 2020 7:06:15 pm
+ * ––––––––––––––––––––––––
+ * Author: Jesus Fermin, 'Pokoi', Villar  (hello@pokoidev.com)
+ * ––––––––––––––––––––––––
+ * MIT License
+ * 
+ * Copyright (c) 2020 Pokoidev
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -12,13 +41,14 @@ public class Cromosome
     static int height   = 2;
     static int length   = 32;
 
-    public float       [] voxels;
-    float              [] mutations;
+    
+    public float       []   voxels;
+    float              []   mutations;
 
-    float       mutationChance      = 0.20f;
-    List<Vector3> navMeshVertices   = new List<Vector3>();
-    float       longestPathDistance = 0.0f;
-    NavMeshAgent agent; 
+    float                   mutationChance  = 0.20f;
+    List<Vector3>           navMeshVertices = new List<Vector3>();
+    float                   longestPathDistance = 0.0f;
+    NavMeshAgent            agent; 
 
 
     public Cromosome()
@@ -99,7 +129,7 @@ public class Cromosome
     public float    GetLongestPathDistance()    => longestPathDistance;
     public void     AddVertex(Vector3 vertex)   => navMeshVertices.Add(vertex);
 
-    public void CalculateLongestDistance(NavMeshAgent agent)
+    public void     CalculateLongestDistance(NavMeshAgent agent)
     {
         this.agent = agent;
 
--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -1,4 +1,34 @@
-﻿using UnityEngine;
+﻿/*
+ * File: Example.cs
+ * File Created: Sunday, 2nd February 2020 7:06:15 pm
+ * ––––––––––––––––––––––––
+ * Author: Jesus Fermin, 'Pokoi', Villar  (hello@pokoidev.com)
+ * ––––––––––––––––––––––––
+ * MIT License
+ * 
+ * Copyright (c) 2020 Pokoidev
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using TMPro;
@@ -12,16 +42,17 @@ using ProceduralNoiseProject;
         public enum EvaluationTypes { pathDistance, navMeshVertices };
         public EvaluationTypes evaluation;
 
-        public Material m_material;
-        const int    numberOfMeshes      = 10;
-        float        visualizationTime   = 0.5f;
-        public int          survivors           = 2;
-        public int          [] seeds            = new int       [numberOfMeshes];
-        Cromosome           [] cromosomes       = new Cromosome [numberOfMeshes];
-        public Transform    [] pivots           = new Transform [numberOfMeshes];
-        List<GameObject>    meshes              = new List<GameObject>();
-        MarchingCubes       marching            = new MarchingCubes();
-        public NavMeshAgent agent;
+        public Material         m_material;
+        public Transform    []  pivots              = new Transform [numberOfMeshes];
+        public NavMeshAgent     agent;
+        
+        const int               numberOfMeshes      = 10;
+        float                   visualizationTime   = 0.5f;
+        public int              survivors           = 2;
+        public int          []  seeds               = new int       [numberOfMeshes];
+        Cromosome           []  cromosomes          = new Cromosome [numberOfMeshes];
+        List<GameObject>        meshes              = new List<GameObject>();
+        MarchingCubes           marching            = new MarchingCubes();
     
         public TMP_Dropdown    timesDropwdown;
         public TextMeshProUGUI GenerationText;
